b44647486f9eb0e9dd19099b12c5c057b24f73a1
Rafa Canosa <rafacv23@gmail.com>
2024-09-03 01:43:09 +0200

    conditionall rendering with the pokemons length

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import HeroSection from "@/components/HeroSection"
 import ThemeTogglebutton from "@/components/ui/theme-togggle"
 import Footer from "@/components/Footer"
+import { usePokemonStore } from "./store/store"
+import Play from "@/components/Play"
 
 function App() {
+  const pokemons = usePokemonStore((state) => state.pokemons)
   return (
     <>
       <div className="fixed top-2 right-6">
         <ThemeTogglebutton />
       </div>
-      <HeroSection />
+      {pokemons.length > 0 ? <Play /> : <HeroSection />}
       <Footer />
     </>
   )
