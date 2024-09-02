bd491d3514d47f4240e43120651c38f430ebd7ab
Rafa Canosa <rafacv23@gmail.com>
2024-09-02 23:31:27 +0200

    feat: create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button"
+import { GithubIcon, LinkedinIcon } from "lucide-react"
+import { githubUrl } from "@/site/config"
+
+export default function Footer() {
+  return (
+    <footer className="grid place-content-center">
+      <div className="space-x-4 mb-4">
+        <Button asChild className="max-w-fit" size={"lg"} variant={"outline"}>
+          <a href={githubUrl} target="_blank" rel="noreferrer">
+            <LinkedinIcon size={20} />
+          </a>
+        </Button>
+        <Button asChild className="max-w-fit" size={"lg"} variant={"outline"}>
+          <a href={githubUrl} target="_blank" rel="noreferrer">
+            <GithubIcon size={20} />
+          </a>
+        </Button>
+      </div>
+      <h2>Created by Rafa Canosa</h2>
+    </footer>
+  )
+}
