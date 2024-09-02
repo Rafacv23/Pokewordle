c34ef854a7f641d4152ba9055198c1ff2de694d2
Rafa Canosa <rafacv23@gmail.com>
2024-09-03 01:34:44 +0200

    new store with zustand

---
+++ src/store/store.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+export interface Pokemon {
+  name: string
+  url: string
+}
+
+export interface State {
+  pokemons: Pokemon[]
+  selectedPokemon: Pokemon | null
+  getAllPokemons: () => Promise<void>
+}
+
+export const usePokemonStore = create<State>()(
+  persist(
+    (set) => ({
+      pokemons: [],
+      selectedPokemon: null,
+      getAllPokemons: async () => {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+        )
+        const data = await response.json()
+
+        console.log("Pokemons received")
+        set({ pokemons: data.results })
+      },
+    }),
+    {
+      name: "pokemon-storage", // unique name
+    }
+  )
+)
