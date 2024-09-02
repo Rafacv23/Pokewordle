bcc1a8c559d55cfc95b8be78e69b3c97c5e2b4e6
Rafa Canosa <rafacv23@gmail.com>
2024-09-03 01:42:48 +0200

    feat: create Play.tsx

---
+++ src/components/Play.tsx
@@ -0,0 +1,16 @@
+import { usePokemonStore } from "@/store/store"
+import { Button } from "./ui/button"
+
+export default function Play() {
+  const reset = usePokemonStore((state) => state.reset)
+  const handleReset = () => {
+    console.log("Borrando datos de los pokemon")
+    reset()
+  }
+  return (
+    <div>
+      Esto es lo que el jugador verá cuando está jugando
+      <Button onClick={handleReset}>Reset</Button>
+    </div>
+  )
+}
