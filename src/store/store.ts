d0ae5338412c2fb67809c3916cefc683247f2802
Rafa Canosa <rafacv23@gmail.com>
2024-09-03 18:49:33 +0200

    special pokemons like megamax or dynamax excluded

--- src/store/store.ts
+++ src/store/store.ts
@@ -78,56 +78,63 @@ export const usePokemonStore = create<State>()(
         const response = await fetch(
           "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
         )
-        const data = await response.json()
 
         if (!response.ok) {
           throw new Error("Failed to fetch data")
         }
 
+        const data = await response.json()
         console.log("Pokemons received")
 
-        // Actualiza el estado con los datos obtenidos
         set({ pokemons: data.results })
 
-        // Obtén la lista de pokemons actualizada
         const pokemons = get().pokemons
 
-        // Selecciona un Pokémon aleatorio
-        const selectedPokemon =
-          pokemons[Math.floor(Math.random() * pokemons.length)]
-
-        console.log(selectedPokemon)
-
-        const res = await fetch(selectedPokemon.url)
-
-        const pokemonData = await res.json()
-
-        const speciesUrl = pokemonData.species.url // To get the generation of the pokemon and the evos
-
-        const re = await fetch(speciesUrl)
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch data")
+        if (pokemons.length === 0) {
+          throw new Error("No pokemons available")
         }
 
-        const speciesData = await re.json()
+        let selectedPokemonData = null
+        while (!selectedPokemonData) {
+          const selectedPokemon =
+            pokemons[Math.floor(Math.random() * pokemons.length)]
+          const res = await fetch(selectedPokemon.url)
 
-        const generation = await speciesData.generation.name
-        const evolvesFrom = await speciesData.evolves_from_species
+          if (!res.ok) {
+            throw new Error("Failed to fetch data")
+          }
 
-        const filteredPokemonData = {
-          id: pokemonData.id,
-          name: pokemonData.name,
-          url: pokemonData.url,
-          weight: pokemonData.weight,
-          height: pokemonData.height,
-          sprites: pokemonData.sprites.front_default,
-          types: pokemonData.types.map((type: PokemonType) => type),
-          generation: generation,
-          evolvesFrom: evolvesFrom ? evolvesFrom.name : null,
+          const pokemonData = await res.json()
+
+          if (pokemonData.is_default) {
+            const speciesUrl = pokemonData.species.url
+            const speciesRes = await fetch(speciesUrl)
+
+            if (!speciesRes.ok) {
+              throw new Error("Failed to fetch data")
+            }
+
+            const speciesData = await speciesRes.json()
+            const generation = speciesData.generation.name
+            const evolvesFrom = speciesData.evolves_from_species
+
+            selectedPokemonData = {
+              id: pokemonData.id,
+              name: pokemonData.name,
+              url: pokemonData.url,
+              weight: pokemonData.weight,
+              height: pokemonData.height,
+              sprites: pokemonData.sprites.front_default,
+              types: pokemonData.types.map(
+                (type: PokemonType) => type.type.name
+              ),
+              generation: generation,
+              evolvesFrom: evolvesFrom ? evolvesFrom.name : null,
+            }
+          }
         }
-        // Actualiza el estado con el Pokémon seleccionado
-        set({ selectedPokemon: filteredPokemonData })
+
+        set({ selectedPokemon: selectedPokemonData })
       },
       selectPokemon: async () => {
         const pokemons = get().pokemons
