46312c6975e13d9dcbeb51502d9f1c1245cadd32
Rafa Canosa <rafacv23@gmail.com>
2024-09-03 18:35:13 +0200

    new function to format strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatString(input: string): string {
+  // Reemplaza los guiones con espacios
+  const replacedString = input.replace(/-/g, " ")
+
+  // Convierte la primera letra a mayúscula y concatena con el resto de la cadena
+  return replacedString.charAt(0).toUpperCase() + replacedString.slice(1)
+}
