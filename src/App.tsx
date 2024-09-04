780d89f9fa9920a772b0e76c93af62343e394154
Rafa Canosa <rafacv23@gmail.com>
2024-09-04 12:43:21 +0200

    win or lose logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import Play from "@/components/Play"
 function App() {
   const pokemons = usePokemonStore((state) => state.pokemons)
   const selectedPokemon = usePokemonStore((state) => state.selectedPokemon)
+  const pokemonsByTheUser = usePokemonStore((state) => state.pokemonsByTheUser)
   return (
     <>
       <div className="fixed top-2 right-6">
         <ThemeTogglebutton />
       </div>
+      {pokemonsByTheUser.find((pokemon) => pokemon.id === selectedPokemon.id)
+        ? "Ganaste" // crear componente para cuando el usuario gana
+        : null}
       {pokemons.length > 0 && selectedPokemon ? <Play /> : <HeroSection />}
       <Footer />
     </>
