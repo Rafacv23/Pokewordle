83722efcbb5368acacef7f359b7653704a89973a
Rafa Canosa <rafacv23@gmail.com>
2024-09-06 17:14:03 +0200

    styles

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,9 @@
 import "../loader.css"
 
 export default function Loader() {
-  return <div className="pokemon"></div>
+  return (
+    <div className="loader-container">
+      <div className="pokemon"></div>
+    </div>
+  )
 }
