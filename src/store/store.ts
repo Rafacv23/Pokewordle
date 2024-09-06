b133828a5c355b7ae789b33680355995141b8f5d
Rafa Canosa <rafacv23@gmail.com>
2024-09-06 17:14:39 +0200

    manage selected generations

--- src/store/store.ts
+++ src/store/store.ts
@@ -27,6 +27,8 @@ export interface State {
   pokemons: Pokemon[]
   selectedPokemon: Pokemon | null
   pokemonsByTheUser: Pokemon[]
+  selectedGenerations: string[]
+  toggleGeneration: (generation: string) => void
   addPokemonsByTheUser: (pokemon: Pokemon) => void
   getAllPokemons: () => Promise<void>
   selectPokemon: () => Promise<void>
@@ -47,31 +49,33 @@ export const usePokemonStore = create<State>()(
       pokemons: [],
       selectedPokemon: null,
       pokemonsByTheUser: [],
+      selectedGenerations: [
+        "generation-i",
+        "generation-ii",
+        "generation-iii",
+        "generation-iv",
+        "generation-v",
+        "generation-vi",
+        "generation-vii",
+        "generation-viii",
+        "generation-ix",
+      ],
+      toggleGeneration: (generation: string) => {
+        set((state) => ({
+          selectedGenerations: state.selectedGenerations.includes(generation)
+            ? state.selectedGenerations.filter((g) => g !== generation)
+            : [...state.selectedGenerations, generation],
+        }))
+      },
+
       addPokemonsByTheUser: async (pokemon: Pokemon) => {
         const res = await fetch(pokemon.url)
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch data")
-        }
-
         const pokemonData = await res.json()
+        const speciesRes = await fetch(pokemonData.species.url)
+        const speciesData = await speciesRes.json()
 
-        const speciesUrl = pokemonData.species.url // To get the generation of the pokemon and the evos
-
-        const response = await fetch(speciesUrl)
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch data")
-        }
-
-        const speciesData = await response.json()
-
-        const generation = await speciesData.generation.name
-        const evolvesFrom = await speciesData.evolves_from_species
-
-        const types = pokemonData.types.map(
-          (type: PokemonType) => type.type.name
-        )
+        const generation = speciesData.generation.name
+        const evolvesFrom = speciesData.evolves_from_species
 
         const filteredPokemonData = {
           id: pokemonData.id,
@@ -80,7 +84,7 @@ export const usePokemonStore = create<State>()(
           weight: pokemonData.weight,
           height: pokemonData.height,
           sprites: pokemonData.sprites.front_default,
-          types: types,
+          types: pokemonData.types.map((type: PokemonType) => type.type.name),
           generation: generation,
           evolvesFrom: evolvesFrom ? evolvesFrom.name : null,
         }
@@ -90,17 +94,15 @@ export const usePokemonStore = create<State>()(
           pokemonsByTheUser: [...state.pokemonsByTheUser, filteredPokemonData],
         }))
       },
+
       getAllPokemons: async () => {
         try {
-          // Activar el estado de loading
           set({ loading: true })
 
-          // Resetear los estados antes de hacer la petición
-          set({
-            turn: 0,
-            selectedPokemon: null,
-            pokemonsByTheUser: [],
-          })
+          const selectedGenerations = get().selectedGenerations
+          if (selectedGenerations.length === 0) {
+            throw new Error("No generations selected")
+          }
 
           const response = await fetch(
             "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
@@ -111,8 +113,8 @@ export const usePokemonStore = create<State>()(
           }
 
           const data = await response.json()
-          console.log("Pokemons received")
 
+          // Almacenar todos los Pokémon en el estado
           set({ pokemons: data.results })
 
           const pokemons = get().pokemons
@@ -122,10 +124,13 @@ export const usePokemonStore = create<State>()(
           }
 
           let selectedPokemonData = null
-          while (!selectedPokemonData) {
-            const selectedPokemon =
+          let attempts = 0
+
+          // Bucle while hasta encontrar un Pokémon de la generación seleccionada
+          while (!selectedPokemonData && attempts < 100) {
+            const randomPokemon =
               pokemons[Math.floor(Math.random() * pokemons.length)]
-            const res = await fetch(selectedPokemon.url)
+            const res = await fetch(randomPokemon.url)
 
             if (!res.ok) {
               throw new Error("Failed to fetch data")
@@ -133,18 +138,16 @@ export const usePokemonStore = create<State>()(
 
             const pokemonData = await res.json()
 
-            if (pokemonData.is_default) {
-              const speciesUrl = pokemonData.species.url
-              const speciesRes = await fetch(speciesUrl)
+            const speciesRes = await fetch(pokemonData.species.url)
+            if (!speciesRes.ok) {
+              throw new Error("Failed to fetch species data")
+            }
 
-              if (!speciesRes.ok) {
-                throw new Error("Failed to fetch data")
-              }
+            const speciesData = await speciesRes.json()
+            const generation = speciesData.generation.name
 
-              const speciesData = await speciesRes.json()
-              const generation = speciesData.generation.name
-              const evolvesFrom = speciesData.evolves_from_species
-
+            // Comprobar si el Pokémon pertenece a una de las generaciones seleccionadas
+            if (selectedGenerations.includes(generation)) {
               selectedPokemonData = {
                 id: pokemonData.id,
                 name: pokemonData.name,
@@ -156,19 +159,27 @@ export const usePokemonStore = create<State>()(
                   (type: PokemonType) => type.type.name
                 ),
                 generation: generation,
-                evolvesFrom: evolvesFrom ? evolvesFrom.name : null,
+                evolvesFrom: speciesData.evolves_from_species
+                  ? speciesData.evolves_from_species.name
+                  : null,
               }
             }
+
+            attempts++
+          }
+
+          if (!selectedPokemonData) {
+            throw new Error("No Pokémon found from the selected generations")
           }
 
           set({ selectedPokemon: selectedPokemonData })
         } catch (error) {
           console.error(error)
         } finally {
-          // Desactivar el estado de loading
           set({ loading: false })
         }
       },
+
       selectPokemon: async () => {
         const pokemons = get().pokemons
         if (pokemons.length > 0) {
@@ -177,12 +188,24 @@ export const usePokemonStore = create<State>()(
           set({ selectedPokemon: selectedPokemon })
         }
       },
+
       reset: () => {
         set({
           pokemons: [],
           selectedPokemon: null,
           pokemonsByTheUser: [],
           turn: 0,
+          selectedGenerations: [
+            "generation-i",
+            "generation-ii",
+            "generation-iii",
+            "generation-iv",
+            "generation-v",
+            "generation-vi",
+            "generation-vii",
+            "generation-viii",
+            "generation-ix",
+          ],
         })
       },
     }),
