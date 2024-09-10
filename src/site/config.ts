74595ea167ad7d6ec1673df34f1fd99739654411
Rafa Canosa <rafacv23@gmail.com>
2024-09-10 17:04:18 +0200

    add production url

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,2 +1,3 @@
 export const githubUrl = "https://github.com/rafacv23/pokewordle"
 export const linkedinUrl = "https://ninjapath.vercel.app/linkedin"
+export const productionUrl = `https://pokemonwordle.vercel.app/`
