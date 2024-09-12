d9f6df975e0925e2f34e6c3d3b59168a555bbcd8
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 01:59:39 +0200

    preview of  the selected generations in the play screen

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -4,6 +4,14 @@ import GuestedPokemons from "@/components/GuestedPokemons"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import SearchPokemon from "@/components/SearchPokemon"
+import { formatString } from "@/lib/utils"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Play() {
   const { t } = useTranslation(["play"])
@@ -11,6 +19,7 @@ export default function Play() {
 
   const pokemonsByTheUser = usePokemonStore((state) => state.pokemonsByTheUser)
   const getAllPokemons = usePokemonStore((state) => state.getAllPokemons)
+  const generations = usePokemonStore((state) => state.selectedGenerations)
 
   const [loading, setLoading] = useState<boolean>(false) // to manage the disabled btn and loading state
 
@@ -25,6 +34,20 @@ export default function Play() {
 
   return (
     <div className="w-full py-12 md:py-24 lg:py-32 bg-background px-2 mx-auto max-w-screen-xl md:px-6">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">
+            <small>{t("selected-generations")}</small>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          {generations.map((generation) => (
+            <DropdownMenuItem key={generation}>
+              {formatString(generation)}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
       {pokemonsByTheUser.length > 0 && <GuestedPokemons />}
       <SearchPokemon loading={loading} setLoading={setLoading} />
       <div className="space-x-4">
